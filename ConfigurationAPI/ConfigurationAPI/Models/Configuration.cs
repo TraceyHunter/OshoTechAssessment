a061ed36d222dc9ce8c5887ba418469090e9d3b4
traceyhunter@yahoo.com <Momod0420>
2022-10-26 16:59:37 +1300

    Configuration class members update after a bit of research.
    Looks like next step is a DTO to have it prograss through configuration state.  Envisioning post to set state to draft and a few more controller methods to move it to current version etc.

--- ConfigurationAPI/ConfigurationAPI/Models/Configuration.cs
+++ ConfigurationAPI/ConfigurationAPI/Models/Configuration.cs
@@ -4,7 +4,11 @@
     {
         public Guid ConfigurationId { get; set; }
         public DateTime CreatedOn { get; set; }
-        public string? Version { get; set; }
-        public string? Location { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? Description { get; set; }
+        public string? ConfigurationState { get; set; }
+        public DateTime? VersionedDate { get; set; }
+        public int? VersionId { get; set; }
+        public DateTime? VersionExpired { get; set; }
     }
 }
